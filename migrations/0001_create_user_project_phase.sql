fecd562affcdde17a7f369fbeef949f5f20cef12
mizphses <fuminori@mizphs.es>
2024-05-08 23:30:54 +0900

    FORCE: change db

--- migrations/0001_create_user_project_phase.sql
+++ migrations/0001_create_user_project_phase.sql
@@ -13,6 +13,8 @@ CREATE TABLE "Project" (
     "id" TEXT NOT NULL PRIMARY KEY,
     "name" TEXT NOT NULL,
     "userId" TEXT NOT NULL,
+    "title" TEXT NOT NULL,
+    "description" TEXT,
     "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     "updated_at" DATETIME NOT NULL,
     CONSTRAINT "Project_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User" ("id") ON DELETE RESTRICT ON UPDATE CASCADE
--- src/projects/index.tsx
+++ src/projects/index.tsx
@@ -1,8 +1,17 @@
 import { Hono } from "hono";
 import type { Env } from "../bindings";
+import { PrismaClient } from "@prisma/client";
+import { PrismaD1 } from "@prisma/adapter-d1";
 
 const projects = new Hono<{ Bindings: Env }>();
 
-projects.get("/", (c) => c.json({ message: "Hello, World!" }));
+projects.get("/", async (c) => {
+	const prisma = new PrismaClient({ adapter: new PrismaD1(c.env.PET_D1) });
+
+	const projects = await prisma.project.findMany();
+	return c.json({
+		projects,
+	});
+});
 // app.route('/users', users)
 export default projects;
